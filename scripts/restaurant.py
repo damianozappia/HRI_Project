8d03ab90249a2db8a24bf5a00c106d9818ba7ea0
damianozappia <damianozappia@gmail.com>
2020-07-15 10:25:19 +0200

    first commit

---
+++ scripts/restaurant.py
@@ -0,0 +1,62 @@
+import sys
+import time
+import os
+import random
+
+try:
+    sys.path.insert(0, os.getenv('MODIM_HOME')+'/src/GUI')
+except Exception as e:
+    print "Please set MODIM_HOME environment variable to MODIM folder."
+    sys.exit(1)
+
+# Set MODIM_IP to connnect to remote MODIM server
+
+import ws_client
+from ws_client import *
+
+def i1():
+
+    im.init()
+
+    im.ask('welcome')  # wait for button
+
+    q = random.choice(['animal','color'])
+
+    a = im.ask(q)
+
+    if (a!='timeout'):
+        im.execute(a)
+        im.execute('goodbye')
+
+    im.init()
+
+def i2():
+
+    im.init()
+
+    im.ask('welcome')
+
+
+    q = ('people')
+    
+    a = im.ask(q)
+
+    if (a!='timeout'):
+        im.execute(a)
+        im.execute('see_you')
+
+    im.init()
+
+
+if __name__ == "__main__":
+
+    mws = ModimWSClient()
+
+    # local execution
+    mws.setDemoPathAuto(__file__)
+    # remote execution
+    # mws.setDemoPath('<ABSOLUTE_DEMO_PATH_ON_REMOTE_SERVER>')
+
+    mws.run_interaction(i2)
+
+
