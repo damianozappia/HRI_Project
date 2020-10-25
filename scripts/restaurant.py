f190c85edc99ee12a7178b3c9fe5ecc3e1b80806
Fabian Fonseca <fhfonsecaa@gmail.com>
2020-10-25 20:41:57 +0100

    implementation and test of AL service api

--- scripts/restaurant.py
+++ scripts/restaurant.py
@@ -203,8 +203,28 @@ def info():
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
     tables = "1 1 1 1 1 1"
     init_tables()
+
+    parser.add_argument("--pip", type=str, default=os.environ['PEPPER_IP'],
+                        help="Robot IP address.  On robot or Local Naoqi: use '127.0.0.1'.")
+    parser.add_argument("--pport", type=int, default=9559,
+                        help="Naoqi port number")
+    args = parser.parse_args()
+    pip = args.pip
+    pport = args.pport
+
+    #Starting application
+    try:
+        connection_url = "tcp://" + pip + ":" + str(pport)
+        app = qi.Application(["ReactToTouch", "--qi-url=" + connection_url ])
+    except RuntimeError:
+        print ("Can't connect to Naoqi at ip \"" + pip + "\" on port " + str(pport) +".\n"
+               "Please check your script arguments. Run with -h option for help.")
+        sys.exit(1)
+
 
     mws = ModimWSClient()
     # f = open("/home/robot/playground/html/sample/logs/tables.txt","w")
@@ -218,6 +238,6 @@ if __name__ == "__main__":
     # mws.setDemoPath('<ABSOLUTE_DEMO_PATH_ON_REMOTE_SERVER>')
 
 
-    #mws.run_interaction(welcome)
-    mws.run_interaction(menu)
+    mws.run_interaction(welcome)
+    # mws.run_interaction(menu)
     #mws.run_interaction(info)
